0d6fd86b981ce72a30b89f0755e8dab4d02a1e70
Quangle <quang221208642@gmail.com>
2026-06-04 22:11:16 +0700

    add exception for vip customer

--- src/entities/VIPCustomer.java
+++ src/entities/VIPCustomer.java
@@ -1,11 +1,14 @@
 package entities;
-
+import utils.Validation;
+import exceptions.InvalidInputException;
 public class VIPCustomer extends Customer {
     private double discountRate;
 
     // Constructor
-    public VIPCustomer(String customerId, String name, String phone, String address, double discountRate) {
+    public VIPCustomer(String customerId, String name, String phone, String address, double discountRate) throws InvalidInputException {
         super(customerId, name, phone, address);
+        Validation.checkNonNegativeDouble(discountRate, "Discount rate");
+        Validation.checkPositiveDouble(discountRate, "Discount rate");
         this.discountRate = discountRate;
     }
 
