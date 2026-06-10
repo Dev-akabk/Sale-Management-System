b3c068bb1b2ea4f49a9fa7a7456ab87858319636
Quangle <quang221208642@gmail.com>
2026-06-10 13:29:06 +0700

    Refactor: improve formatting and comments in OrderDetail class

--- src/models/OrderDetail.java
+++ src/models/OrderDetail.java
@@ -1,32 +1,28 @@
 package models;
-
+//nhiem vu file nay la luu 1 dong thong tin don hang: san pham + so luong
 public class OrderDetail {
+
     private Product product;
     private int quantity;
 
-    // Constructor
+    // Constructor--------
     public OrderDetail(Product product, int quantity) {
         this.product = product;
         this.quantity = quantity;
     }
 
-    // Getters & Setters
-    public Product getProduct() {
-        return product;
-    }
+    // Getters & Setters------
+    public Product getProduct()              { return product; }
+    public void    setProduct(Product p)     { this.product = p; }
 
-    public void setProduct(Product product) {
-        this.product = product;
+    public int     getQuantity()             { return quantity; }
+    public void    setQuantity(int quantity) { this.quantity = quantity; }
+    
+    //Total------
+    public double getLineTotal() {
+        return product.getPrice() * quantity;
     }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
+    //toString-----
     @Override
     public String toString() {
         return "OrderDetail{" + "product=" + product.getProductName() + ", quantity=" + quantity + '}';
